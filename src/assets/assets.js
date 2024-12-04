cd762bbd6b7f57c2d058e3ed6f84d945b50d4142
Babu <veldevi2004@gmail.com>
2024-12-04 19:34:45 +0530

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,75 @@
+import project_img_1 from './project_img_1.jpg'
+import project_img_2 from './project_img_2.jpg'
+import project_img_3 from './project_img_3.jpg'
+import project_img_4 from './project_img_4.jpg'
+import project_img_5 from './project_img_5.jpg'
+import project_img_6 from './project_img_6.jpg'
+import profile_img_1 from './profile_img_1.jpg'
+import profile_img_2 from './profile_img_2.jpg'
+import profile_img_3 from './profile_img_3.jpg'
+
+export const projectData=[
+    {
+        title:"Skyline Haven",
+        price:"2,50,000",
+        location:"Chennai",
+        image:project_img_1
+    },
+    {
+        title:"Vista Verde",
+        price:"2,00,000",
+        location:"Kerala",
+        image:project_img_2
+    },
+    {
+        title:"Serenity Suites",
+        price:"3,50,000",
+        location:"Mumbai",
+        image:project_img_3
+    },
+    {
+        title:"Central Square",
+        price:"3,00,000",
+        location:"Bangalore",
+        image:project_img_4
+    },
+    {
+        title:"Picasso Villa",
+        price:"3,20,000",
+        location:"Delhi",
+        image:project_img_5
+    },
+    {
+        title:"BeachHouse",
+        price:"4,50,000",
+        location:"Lanka",
+        image:project_img_6
+    }
+]
+
+export const testimonialsData=[
+    {
+        name:"Will Jack",
+        title:"Marketing Manager",
+        image:profile_img_1,
+        alt:"Portrait of Will Jack",
+        rating:5,
+        text:"Will Jack expertise in the market was evident from our first meeting. They provided a detailed analysis of market trends, helping us understand the best time to buy. For example, they accurately predicted a price drop in our preferred neighborhood, allowing us to make a timely purchase."
+    },
+    {
+        name:"Jacob Bathol",
+        title:"Founder",
+        image:profile_img_2,
+        alt:"Portrait of Jacob Bathol",
+        rating:4,
+        text:"Throughout our home-buying process, Jacob Bathol was incredibly communicative. They promptly answered calls and emails, keeping us informed about every development. Their ability to clearly explain complex contract details was particularly helpful."
+    },
+    {
+        name:"James Andreson",
+        title:"Co-Founder",
+        image:profile_img_3,
+        alt:"Portrait of James Andreson",
+        rating:3,
+        text:"We were impressed by James Andreson negotiation skills. They managed to lower the asking price significantly, saving us thousands. Their strategy during the negotiation phase was impressive, as they leveraged their market knowledge to our advantage."
+    }
+]
